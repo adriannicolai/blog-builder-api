0f85a87ab28264576d0269a7b70411e9666f3fab
adriannicolai <anlumbad@gmail.com>
2022-09-26 15:06:51 +0800

    add checker for existing user

--- server/app/helpers/user_helper.rb
+++ server/app/helpers/user_helper.rb
@@ -19,8 +19,13 @@ module UserHelper
             params.each do |key, value|
                 validation_key = [:first_name, :last_name].include?(key) ? :name : key
 
+                # Validate password matching
                 if validation_key === :password && params[:confirm_password].present?
-                    response_data[:password] = "Passwords do not match" if value != params[:confirm_password]
+                    response_data[:result][validation_key] = "Passwords do not match" if value != params[:confirm_password]
+
+                # Validate if user is already registered
+                elsif validation_key === :email
+                    response_data[:result][validation_key] = "User is already registered" if User.get_user_record({ :fields_to_filter => { :email => value } })[:status]
                 end
 
                 # Skip theh regex mathing if the key is not present in the validations hash
@@ -31,14 +36,13 @@ module UserHelper
 
                 response_data[:result][validation_key] = validations[validation_key][:error] if validation_key === :name ? !validation_comparison.nil? : validation_comparison.nil?
             end
+
+            # Return the status if the result is empty
+            response_data[:status] = true if response_data[:result].empty?
         rescue Exception => ex
             response_data[:error] = ex.message
         end
 
-        p "response_data"
-        p response_data
-        p "response_data"
-        p "response_data"
         return response_data
     end
 end
