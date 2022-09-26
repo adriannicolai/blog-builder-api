236b67cea4b6e06e1c33843c4df72d4fb1a7a049
adriannicolai <anlumbad@gmail.com>
2022-09-26 15:26:49 +0800

    add creation of user in model

--- server/app/models/user.rb
+++ server/app/models/user.rb
@@ -1,7 +1,46 @@
 include QueryHelper
 class User < ApplicationRecord
+        # DOCU: Function to get user record dynamically
+        # Triggered by: UserController
+        # Requires: params - first_name, last_name, email, password, user_level
+        # Last updated at: September 26, 2022
+        # Owner: Adrian
+        def self.create_new_user(params)
+            response_data = { :status => false, :result => {}, :error => nil }
+
+            begin
+              	# Check for valid fields
+                check_register_form_fields = check_fields(["email", "password", "confirm_password", "first_name", "last_name"], [], params)
+
+                if check_register_form_fields[:status]
+                    # Proceed to registering the user if all fields are valid
+                    validate_new_user_info = validate_new_user_info(check_register_form_fields[:result])
+
+                    if validate_new_user_info[:status]
+                        # Destructure check_register_form_fields
+                        first_name, last_name, email, password = check_register_form_fields.values_at(:first_name, :last_name, :email, :password)
+
+                        # Create a new user
+                        create_user_id = "
+                            INSERT INTO users (first_name, last_name, email, password, user_level, created_at, updated_at)
+                            VALUES(?, ?, ?, ?, ?, NOW(), NOW())
+                        ", first_name, last_name, email, password, USER_LEVEL_ID[:admin]
+                    else
+                        response_data.merge!(validate_new_user_info)
+                    end
+                else
+                    response_data.merge!(check_register_form_fields)
+			    end
+            rescue Exception => ex
+                response_data[:error] = ex.message
+            end
+
+            return response_data
+
+        end
+
     private
-        # DOCU: Function to get user record dynamiccally
+        # DOCU: Function to get user record dynamically
         # Triggered by: UserController
         # Requires: params - fields_to_filter
         # Optionals: params - fields_to_select
