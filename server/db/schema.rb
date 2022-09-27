20966f66a78c7e0818066fc5896b8af3afa19513
adriannicolai <anlumbad@gmail.com>
2022-09-27 09:42:42 +0800

    added blog titles

--- server/db/schema.rb
+++ server/db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_27_012922) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_27_014111) do
+  create_table "blog_titles", charset: "utf8mb3", force: :cascade do |t|
+    t.bigint "blogs_id", null: false
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["blogs_id"], name: "index_blog_titles_on_blogs_id"
+  end
+
   create_table "blogs", charset: "utf8mb3", force: :cascade do |t|
     t.bigint "users_id", null: false
     t.string "name"
@@ -30,5 +38,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_27_012922) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "blog_titles", "blogs", column: "blogs_id"
   add_foreign_key "blogs", "users", column: "users_id"
 end
