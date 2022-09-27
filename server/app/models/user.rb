b3769a19fa237527b84faba80e7b4c3ebbe626ed
adriannicolai <anlumbad@gmail.com>
2022-09-27 15:23:42 +0800

    cleaned indentation

--- server/app/models/user.rb
+++ server/app/models/user.rb
@@ -2,57 +2,57 @@ include ApplicationHelper
 include UserHelper
 include QueryHelper
 class User < ApplicationRecord
-        # DOCU: Function to get user record dynamically
-        # Triggered by: UserController
-        # Requires: params - first_name, last_name, email, password, user_level
-        # Last updated at: September 27, 2022
-        # Owner: Adrian
-        def self.create_new_user(params)
-            response_data = { :status => false, :result => {}, :error => nil }
+    # DOCU: Function to get user record dynamically
+    # Triggered by: UserController
+    # Requires: params - first_name, last_name, email, password, user_level
+    # Last updated at: September 27, 2022
+    # Owner: Adrian
+    def self.create_new_user(params)
+        response_data = { :status => false, :result => {}, :error => nil }
 
-            begin
-              	# Check for valid fields
-                check_register_form_fields = check_fields(["email", "password", "confirm_password", "first_name", "last_name"], [], params)
+        begin
+            # Check for valid fields
+            check_register_form_fields = check_fields(["email", "password", "confirm_password", "first_name", "last_name"], [], params)
 
-                if check_register_form_fields[:status]
-                    # Proceed to registering the user if all fields are valid
-                    validate_new_user_info = validate_new_user_info(check_register_form_fields[:result])
+            if check_register_form_fields[:status]
+                # Proceed to registering the user if all fields are valid
+                validate_new_user_info = validate_new_user_info(check_register_form_fields[:result])
 
-                    if validate_new_user_info[:status]
-                        # Destructure check_register_form_fields
-                        first_name, last_name, email, password = check_register_form_fields[:result].values_at(:first_name, :last_name, :email, :password)
+                if validate_new_user_info[:status]
+                    # Destructure check_register_form_fields
+                    first_name, last_name, email, password = check_register_form_fields[:result].values_at(:first_name, :last_name, :email, :password)
 
-                        # Create a new user
-                        created_user_id = insert_record(["
-                            INSERT INTO users (first_name, last_name, email, password, user_level, created_at, updated_at)
-                            VALUES(?, ?, ?, ?, ?, NOW(), NOW())
-                        ", first_name.titleize, last_name.titleize, email.downcase, encrypt_password(password), USER_LEVEL_ID[:admin]])
+                    # Create a new user
+                    created_user_id = insert_record(["
+                        INSERT INTO users (first_name, last_name, email, password, user_level, created_at, updated_at)
+                        VALUES(?, ?, ?, ?, ?, NOW(), NOW())
+                    ", first_name.titleize, last_name.titleize, email.downcase, encrypt_password(password), USER_LEVEL_ID[:admin]])
 
-                        if created_user_id.present?
-                            # Fetch created user
-                            # TODO: Add encryption of user details
-                            user_details = self.get_user_record({ :fields_to_filter => { :id => created_user_id } })
+                    if created_user_id.present?
+                        # Fetch created user
+                        # TODO: Add encryption of user details
+                        user_details = self.get_user_record({ :fields_to_filter => { :id => created_user_id } })
 
-                            # Create a new vlog
-                            blog_details = Blog.create_blog({:user_id => created_user_id})
+                        # Create a new vlog
+                        blog_details = Blog.create_blog({:user_id => created_user_id})
 
-                            response_data.merge!(blog_details[:status] ? { :result => { :user_details => user_details, :blog_details => blog_details[:result] }} : { :error => "Error in creating user and blog record. Please try again later." })
-                        else
-                            raise "Error in creating user, Please try again later."
-                        end
+                        response_data.merge!(blog_details[:status] ? { :result => { :user_details => user_details, :blog_details => blog_details[:result] }} : { :error => "Error in creating user and blog record. Please try again later." })
                     else
-                        response_data.merge!(validate_new_user_info)
+                        raise "Error in creating user, Please try again later."
                     end
                 else
-                    response_data.merge!(check_register_form_fields)
-			    end
-            rescue Exception => ex
-                response_data[:error] = ex.message
+                    response_data.merge!(validate_new_user_info)
+                end
+            else
+                response_data.merge!(check_register_form_fields)
             end
-
-            return response_data
-
+        rescue Exception => ex
+            response_data[:error] = ex.message
         end
+
+        return response_data
+
+    end
 
     private
         # DOCU: Function to get user record dynamically
