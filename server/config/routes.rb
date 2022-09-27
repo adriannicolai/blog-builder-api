a50fcf156236961d0369f72629ba0512d01a153d
adriannicolai <anlumbad@gmail.com>
2022-09-27 09:28:01 +0800

    inital commit for routes in blog

--- server/config/routes.rb
+++ server/config/routes.rb
@@ -5,4 +5,12 @@ Rails.application.routes.draw do
 		post "register"	   => "user#register"
 		post "update_user" => "user#update_user"
 	end
+
+	# Route for blogs
+	scope "blog" do
+		get  "view_blog"   => "blog#view_blog"
+
+		post "create_blog" => "blog#create_blog"
+		post "update_log"  => "blog#update_log"
+	end
 end
