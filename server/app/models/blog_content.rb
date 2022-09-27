d4032bf7c4f24b21139b387ff904e270c04fe567
adriannicolai <anlumbad@gmail.com>
2022-09-27 17:19:11 +0800

    add creation of blog content

--- server/app/models/blog_content.rb
+++ server/app/models/blog_content.rb
@@ -29,10 +29,50 @@ class BlogContent < ApplicationRecord
 				INSERT INTO blog_contents (blog_id, blog_title_id, content, created_at, updated_at) VALUES(?, ?, ?, NOW(), NOW())
 			", blog_id, blog_title_id, ""])
 
+			if created_blog_title_content_id.present?
+				response_data.merge!(self.get_blog_content_record({ :fields_to_filter => { :id => created_blog_title_content_id }}))
+			else
+				raise "Error in creating blog_content_record, Please try again later."
+			end
+
 		rescue Exception => ex
 			response_data[:error] = ex.message
 		end
 
 		return response_data
     end
+
+	private
+		# DOCU: Function to fetch blog_content_record dynamically
+		# Triggered by: BlogContentModel
+		# Requires: params - fields_to_filter
+		## Optionals: params - fields_to_select
+		# Last updated at: September 27, 2022
+		# Owner: Adrian
+		def self.get_blog_content_record(params)
+			response_data = { :status => false, :result => {}, :error => nil }
+
+			begin
+				params[:fields_to_select] ||= "*"
+
+				select_blog_query = ["
+					SELECT #{ActiveRecord::Base.sanitize_sql(params[:fields_to_select])}
+					FROM blog_contents WHERE
+				"]
+
+				params[:fields_to_filter].each_with_index do |(field, value), index|
+					select_blog_query[0] += " #{'AND' if index > 0} #{field} #{field.is_a?(Array) ? 'IN(?)' : '=?'}"
+					select_blog_query    << value
+				end
+
+				# Query the blog record
+				blog_record = query_record(select_blog_query)
+
+				response_data.merge!(blog_record.present? ? { :status => true, :result => blog_record } : { :error => "Error in fetching blog_content record data, Please try again later." })
+			rescue Exception => ex
+				response_data[:error] = ex.message
+			end
+
+			return response_data
+		end
 end
