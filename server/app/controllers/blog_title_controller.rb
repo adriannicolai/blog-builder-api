9ac6cf7518cf4d2af7c6d3bf5d50e5bc1383c4f1
adriannicolai <anlumbad@gmail.com>
2022-09-27 15:22:17 +0800

    add creation of blog title

--- server/app/controllers/blog_title_controller.rb
+++ server/app/controllers/blog_title_controller.rb
@@ -1,6 +1,20 @@
 class BlogTitleController < ApplicationController
+	# DOCU: Function for creating blog_title
+    # Triggered by: (POST) /blog_title/create_blog_title
+    # Last updated at: September 27, 2022
+    # Owner: Adrian
 	def create_blog_title
+		response_data = { :status => false, :result => {}, :error => nil }
 
+		begin
+			create_blog_title = BlogTitle.create_blog_title(params)
+
+			response_data.merge!(create_blog_title)
+		rescue Exception => ex
+			response_data[:error] = ex.message
+		end
+
+		render :json => response_data
 	end
 
 	def update_blog_title
