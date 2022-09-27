97ca4995fbbcb9d0028342d4a44f5b43a8538e4a
adriannicolai <anlumbad@gmail.com>
2022-09-27 15:07:48 +0800

    added routes for blog_title

--- server/config/routes.rb
+++ server/config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'blog_title/create_blog_title'
+  get 'blog_title/update_blog_title'
 	# Route for users
 	scope "user" do
 		post "login"	   => "user#login"
@@ -12,5 +14,10 @@ Rails.application.routes.draw do
 
 		post "create_blog" => "blog#create_blog"
 		post "update_log"  => "blog#update_log"
+	end
+
+	scope "blog_title" do
+		post "create_blog_title" => "blog_title#create_blog_title"
+		post "update_blog_title" => "blog_title#update_blog_title"
 	end
 end
