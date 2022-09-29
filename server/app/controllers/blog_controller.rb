f21869616acec95e8d52c399ec89b19c9273262d
adriannicolai <anlumbad@gmail.com>
2022-09-29 10:26:25 +0800

    add controller for fetching blog

--- server/app/controllers/blog_controller.rb
+++ server/app/controllers/blog_controller.rb
@@ -1,6 +1,21 @@
 class BlogController < ApplicationController
+	# DOCU: Function for user login
+    # Triggered by: (POST) /user/register
+	# Requires: params - blog_id
+    # Last updated at: September 27, 2022
+    # Owner: Adrian
 	def view_blog
+		response_data = { :status => false, :result => {}, :error => nil }
 
+		begin
+			blog_details = Blog.get_blog_data(params)
+
+			response_data.merge!(blog_details)
+		rescue Exception => ex
+			response_data[:error] = ex.message
+		end
+
+		render :json => response_data
 	end
 
 	def update_blog
